f40ab794bbeadb1c00ee68157b297f22dea712fc
medine ozkanli <ozknlimedine@gmail.com>
2019-12-10 20:55:21 +0300

    Add project files.

---
+++ RazorPagesSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorPagesSample.Data;
+
+namespace RazorPagesSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Add(new Customer
+                {
+                    Name ="Medişko"
+                });
+                db.Add(new Customer
+                {
+                    Name = "Busoş"
+                });
+                db.SaveChanges();
+            }
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
